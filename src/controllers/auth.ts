2f34638efbf80c5db3fc7b48ed541f062b476d25
SKaLo06 <skalo@gmail.com>
2023-03-03 12:30:53 +0100

    auth yup function

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -28,22 +28,29 @@ exports.login = async (req: Request, res: Response) => {
     const { email, password } = req.body;
     const authSchema = yup.object({
         email: yup.string().email().required(),
-        password: yup.string(),
+        password: yup.string().required(),
     });
+    const validation = async (obj: object) => {
+        try {
+            await authSchema.validate(obj);
+        } catch (error: any) {
+            return res.status(500).json({ success: false, error: error.message });
+        }
+    };
+    validation({ email, password });
     try {
-        const authValidation = await authSchema.validate(email, password);
         const user = await User.findOne({ email }).select('+password');
         if (!user) {
             res.status(401).json({ success: false, error: 'invalid credentials' });
         }
         await bcrypt.compare(password, user.password, function (err: any, ismatch: any) {
             if (err) {
-                res.status(400).json({ success: false, message: 'unmatched', authValidation });
+                res.status(400).json({ success: false, message: 'invalid credentials' });
             }
             if (ismatch) {
                 res.status(200).json({ success: true, message: 'matched' });
             } else {
-                res.status(400).json({ success: false, message: 'unmatched' });
+                res.status(400).json({ success: false, message: 'invalid credentials' });
             }
         });
     } catch (error: any) {
