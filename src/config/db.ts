d12f88727c6a09ce4b08c5ba4850c20d6de20961
SKaLo06 <skalo@gmail.com>
2023-03-03 12:09:11 +0100

    yup

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
+    mongoose.set('strictQuery', false);
     await mongoose.connect(process.env.MONGO_URI);
     console.log('mongo connected');
 };
--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,20 +1,18 @@
 import { Request, Response } from 'express';
 const User = require('../models/User');
+const yup = require('yup');
+const bcrypt = require('bcryptjs');
 
 exports.register = async (req: Request, res: Response) => {
     if (false) {
         console.log(req);
     }
-    
-    const objUsers = {
-        username: 'taha',
-        email: 'tata@gmail.com',
-        password: 'fqfqpf',
-    };
+
+    const { username, email, password } = req.body;
     try {
-        const user = User.create(objUsers);
-        await user.save();
-        res.status(200).json({
+        const passwordHashed = await bcrypt.hash(password, 10);
+        const user = await User.create({ username, email, password: passwordHashed });
+        res.status(201).json({
             success: true,
             user,
         });
@@ -26,9 +24,29 @@ exports.register = async (req: Request, res: Response) => {
     }
     res.send('register');
 };
-exports.login = (req: Request, res: Response) => {
-    if (false) {
-        console.log(req);
+exports.login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const authSchema = yup.object({
+        email: yup.string().email().required(),
+        password: yup.string(),
+    });
+    try {
+        const authValidation = await authSchema.validate(email, password);
+        const user = await User.findOne({ email }).select('+password');
+        if (!user) {
+            res.status(401).json({ success: false, error: 'invalid credentials' });
+        }
+        await bcrypt.compare(password, user.password, function (err: any, ismatch: any) {
+            if (err) {
+                res.status(400).json({ success: false, message: 'unmatched', authValidation });
+            }
+            if (ismatch) {
+                res.status(200).json({ success: true, message: 'matched' });
+            } else {
+                res.status(400).json({ success: false, message: 'unmatched' });
+            }
+        });
+    } catch (error: any) {
+        res.status(500).json({ success: false, error: error.message });
     }
-    res.send('login');
 };
