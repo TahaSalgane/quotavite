d9afcb88c21ccda60870d0277ffa969b6fc4ba49
skalo06 <taha.000.kech@gmail.com>
2023-03-12 14:25:08 +0100

    Simulation of handleError inside  login action

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,29 +1,32 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import User from '../models/User';
 import { yupValidation, authSchema } from '../utils/YupValidation';
 import jwt from 'jsonwebtoken';
 import * as bcrypt from 'bcryptjs';
-import { UserDataInterface } from '../utils/type';
+import { ResponseError, UserDataInterface } from '../utils/type';
 
 const secret = process.env.SECRET_OR_KEY_JWT ?? '';
 
-const register = async (req: Request, res: Response) => {
+const register = async (req: Request, res: Response, next: NextFunction) => {
     const { username, email, password } = req.body;
     try {
+        // if (true) {
+        //     const error = new Error('msg error - exception');
+        //     (error as ResponseError).statusCode = 500;
+        //     throw error;
+        // }
         const passwordHashed = await bcrypt.hash(password, 10);
         const user = await User.create({ username, email, password: passwordHashed });
         res.status(201).json({
             success: true,
             user,
         });
-    } catch (error: any) {
-        res.status(500).json({
-            success: false,
-            error: error.message,
-        });
+    } catch (error: Error | ResponseError | any) {
+        next(error);
+        // return res.status(500).json({ success: false, error: error.message });
     }
 };
-const login = async (req: Request, res: Response) => {
+const login = async (req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body;
     const bodyValidation = await yupValidation(authSchema, { email, password });
     if (!bodyValidation.ok) return res.json(bodyValidation.error.message);
@@ -31,7 +34,10 @@ const login = async (req: Request, res: Response) => {
         const user = await User.findOne({ email }).select('+password');
 
         if (!user) {
-            return res.status(401).json({ success: false, error: 'invalid credentials' });
+            const error = new Error('invalid credentials');
+            (error as ResponseError).statusCode = 400;
+            throw error;
+            // return res.status(400).json({ success: false, error: 'invalid credentials' });
         }
         await bcrypt.compare(password, user?.password, function (err: any, ismatch: any) {
             if (err) {
@@ -53,8 +59,9 @@ const login = async (req: Request, res: Response) => {
                 return res.status(400).json({ success: false, message: 'invalid credentials' });
             }
         });
-    } catch (error: any) {
-        return res.status(500).json({ success: false, error: error.message });
+    } catch (error: Error | ResponseError | any) {
+        next(error);
+        // return res.status(500).json({ success: false, error: error.message });
     }
 };
 export { login, register };
